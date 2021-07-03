4c26f4b5f0abebeedcddd863957bb0c9e3fcc01a
kinson <swilkinsonhunter@gmail.com>
2021-07-03 16:27:55 -0600

    improve test structure

--- test/fiat_cache_test.exs
+++ test/fiat_cache_test.exs
@@ -1,23 +1,22 @@
 defmodule FiatCacheTest do
   use ExUnit.Case
-  doctest FiatCache
+  doctest FiatCache.Server
+
+  alias FiatCache.Server
+
+  setup do
+    Server.start_link()
+    :ok
+  end
 
   test "caches an item" do
-    FiatCache.start_link()
-
-    assert FiatCache.cache_object("dog", {"Henry", 4}) == true
-    assert FiatCache.fetch_object("dog") == {"Henry", 4}
-
-    FiatCache.stop()
+    assert Server.cache_object("dog", {"Henry", 4}) == true
+    assert Server.fetch_object("dog") == {"Henry", 4}
   end
 
   test "queries item if object is not in fetch" do
-    FiatCache.start_link()
-
-    assert FiatCache.fetch_object("dog", fn () -> {"Henry", 5} end) == {"Henry", 5}
-    assert FiatCache.fetch_object("dog") == {"Henry", 5}
-    assert FiatCache.fetch_object("dogg") == nil
-
-    FiatCache.stop()
+    assert Server.fetch_object("dog", fn () -> {"Henry", 5} end) == {"Henry", 5}
+    assert Server.fetch_object("dog") == {"Henry", 5}
+    assert Server.fetch_object("dogg") == nil
   end
 end
